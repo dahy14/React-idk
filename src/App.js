cff861812496f4f6ffe5794039a57ac8da36a977
John Louis Naypes <naypes.15@gmail.com>
2022-04-07 22:21:36 +0800

    File practice 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar.js';
+
+class App extends React.Component {
+
+state = {}
+
+    render() { 
+        return (
+            <div>
+                <h1>Hello! Pekora is Win</h1>
+            </div>
+        )
+    }
+}
+
+
+export default App;
