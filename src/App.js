7b05f8588cc1eb74ac929b923dc555727219e777
John Louis Naypes <naypes.15@gmail.com>
2022-04-09 00:54:15 +0800

    v 2.1 working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App () {
     return (
-        <BrowserRouter>
+        <Router>
+           
             <div className = "App">
                 <Navbar />
-                <Route path='/' component = {Home} /> 
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/contact" element={<Contact/>} />
+            </Routes>
             </div>
-        </BrowserRouter>
+        </Router>
     );
 }
 
