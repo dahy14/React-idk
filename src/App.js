af98a079f34c97f9639670aab349dbf3593d150e
John Louis Naypes <naypes.15@gmail.com>
2022-04-09 09:02:18 +0800

    v 2.1 na may comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
+//This should be comment on the new version ok ok
+// Boy I, boy I know, I know you got the feels
+// Boy I, boy I know. I know you feel it too. 
 
 function App () {
     return (
