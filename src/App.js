62e0892798ea728b494e8ee88f6bb04effd00de4
John Louis Naypes <naypes.15@gmail.com>
2022-04-09 09:07:16 +0800

    v 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Contact from './components/Contact';
 //This should be comment on the new version ok ok
 // Boy I, boy I know, I know you got the feels
 // Boy I, boy I know. I know you feel it too. 
-
+// Ah! I'm so curious but your boy gotta keep it cool
+// but I know everytime you move got me frozen I get
+// so shy its obvious
+ 
 function App () {
     return (
         <Router>
