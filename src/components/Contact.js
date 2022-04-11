3e3f97664aa82157649d475e20a58f359da376f3
John Louis Naypes <naypes.15@gmail.com>
2022-04-11 21:33:56 +0800

    v 1. Create Portal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from './Modal';
 
 const Contact = () => {
     return(
@@ -6,6 +7,7 @@ const Contact = () => {
             className="ui raised very padded text container segment" 
             style={{marginTop:'80px'}}
         >
+            <Modal />
             <h3 className="ui header"> Contacts </h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam atque ducimus commodi distinctio praesentium enim quos quia maxime, nostrum vel culpa eos alias earum error debitis assumenda! Suscipit, exercitationem voluptatibus? Neque est quibusdam incidunt mollitia maiores voluptatem provident, deserunt, deleniti repellat perferendis delectus?</p>
         </div>
